a909ef1f4011f0aff5e8837052867d4d83a7e909
Sudeep Takkar <sudeep.takkar@yahoo.com>
2016-05-17 08:28:03 +0530

    Add files via upload

---
+++ ProductApi/src/ProductApi/Models/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductApi.Models
+{
+    public interface IProductRepository
+    {
+        bool Add(ProductItem item, int Token, string Password);
+        IEnumerable<ProductItem> GetAll(int Token, string User);
+        int GetToken(string User);
+        ProductItem Find(string key, int Token, string Password);
+        ProductItem Remove(string key, int Token, string Password);
+        bool Update(ProductItem item, int Token, string Password);
+    }
+}
---
+++ ProductApi/src/ProductApi/Models/ProductItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductApi.Models
+{
+    public class ProductItem
+    {
+        public string Key { get; set; }
+        public string Name { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+        public double Price { get; set; }
+        public string Color { get; set; }
+        public string Discount { get; set; }
+        public int Token { get; set; }
+        public string IsAvailable { get; set; }
+    }
+}
